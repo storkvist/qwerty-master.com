eab75ee10377e4fb77791fe5665fb62b8674ec4d
Vitaly Shlyaga <storkvist@storkvist.net>
2017-04-06 00:04:39 +0300

    Fix namespace issue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ SimpleCov.start
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport
+module ActiveSupport
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests
     # in alphabetical order.
